ee6d34af1e268b1233709d359869c9d594fcb224
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-09 15:50:28 +0600

    all ssearch

--- phone-hunter/phone-hunter.js
+++ phone-hunter/phone-hunter.js
@@ -1,19 +1,31 @@
-const loadPhones = async(searchText) =>{
+const loadPhones = async(searchText, dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const phones = await res.json();
-    diplayPhones(phones.data);
+    diplayPhones(phones.data, dataLimit);
 }
 
+const showAll = document.getElementById('show-all');
+console.log(showAll);
 loadPhones();
 
-const diplayPhones = (phones)=>{
+const diplayPhones = (phones, dataLimit)=>{
     console.log(phones);
     let holder = document.getElementById('phone-holder');
-    
-    console.log(phones);
-    phones = phones.slice(0,2);
-    console.log(phones);
+   console.log(phones.length);
+    if(dataLimit && phones.length> 10){
+        phones = phones.slice(0,5);
+        showAll.classList.remove('hidden');
+        console.log(showAll);
+    }
+
+   
+
+    else 
+        {
+            showAll.classList.add('hidden');
+
+        }
     phones.forEach(phone => {
         const eachPhone = document.createElement('div'); 
         eachPhone.innerHTML = `
@@ -22,14 +34,14 @@ const diplayPhones = (phones)=>{
         <img src='${phone.image}'>
         <p>
             Lorem ipsum, consectetur adipisicing elit. Neque non quia nesciunt impedit, fuga minima eum perferendis quos ducimus voluptates molestiae, omnis, ullam id hic. Consectetur repellendus doloribus at facere?
-        </p>
-        
-       
+        </p> 
         `
         eachPhone.classList.add('p-12');
         holder.appendChild(eachPhone);
         
     })
+    // spinner off 
+    toggleSpinner(false);
 }
 
 
@@ -38,8 +50,32 @@ searchBtn.addEventListener('click', function(){
     searchBtn.style.backgroundColor = 'tomato';
     const searchText = document.getElementById('search-field').value;
     loadPhones(searchText);
-    
+    toggleSpinner(true);
+    processSearch(5);
 
     
+});
+
+
+
+const toggleSpinner = isLoading => {
+    const loaderSection =  document.getElementById('spinner');
+    
+    if(isLoading){
+        loaderSection.classList.remove('hidden');
+    }
+
+    else{
+        loaderSection.classList.add('hidden');
+    }
+}
+
+document.getElementById('show-all').addEventListener('click', function () {
+    processSearch();
 })
 
+const processSearch = (dataLimit) => {
+    const searchText = document.getElementById('search-field').value;
+    loadPhones(searchText, dataLimit);
+    toggleSpinner(true);
+}
