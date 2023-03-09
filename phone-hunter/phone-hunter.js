d8106323ec3fd537ac379b872102e4dae8b3889f
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-09 11:39:56 +0600

    spinner

--- phone-hunter/phone-hunter.js
+++ phone-hunter/phone-hunter.js
@@ -1,5 +1,5 @@
-const loadPhones = async() =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+const loadPhones = async(searchText) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const phones = await res.json();
     diplayPhones(phones.data);
@@ -9,23 +9,37 @@ loadPhones();
 
 const diplayPhones = (phones)=>{
     console.log(phones);
-    const holder = document.getElementById('phone-holder');
+    let holder = document.getElementById('phone-holder');
+    
+    console.log(phones);
+    phones = phones.slice(0,2);
+    console.log(phones);
     phones.forEach(phone => {
-        const eachPhone = document.createElement('div');
-        
+        const eachPhone = document.createElement('div'); 
         eachPhone.innerHTML = `
         <h1>${phone.brand}</h1>
-        <h2 class = "text-xl"> ${phone.phone_name}</h2>
+        <h2 class = "text-3xl"> ${phone.phone_name}</h2>
         <img src='${phone.image}'>
+        <p>
+            Lorem ipsum, consectetur adipisicing elit. Neque non quia nesciunt impedit, fuga minima eum perferendis quos ducimus voluptates molestiae, omnis, ullam id hic. Consectetur repellendus doloribus at facere?
+        </p>
         
-        
-        
-        
-        
+       
         `
         eachPhone.classList.add('p-12');
-    
         holder.appendChild(eachPhone);
         
     })
 }
+
+
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', function(){
+    searchBtn.style.backgroundColor = 'tomato';
+    const searchText = document.getElementById('search-field').value;
+    loadPhones(searchText);
+    
+
+    
+})
+
